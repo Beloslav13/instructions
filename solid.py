0dc1fa15c364a568c19319f18e7348be025a40a1
beloslav <velislav1329@gmail.com>
2021-02-18 21:12:53 +0300

    add solid OCP

--- solid.py
+++ solid.py
@@ -35,3 +35,53 @@ class AnimalDB:
 
     def save(self, animal: Animal):
         pass
+
+
+"""
+Open Close Principle
+Программные объекты (классы, модули, функции) должны быть открыты для расширения и закрыты для модификации.
+"""
+
+# Не правильноый подход.
+
+class Discount:
+
+    def __init__(self, customer, price):
+        self.customer = customer
+        self.price = price
+
+    def give_discount(self):
+            if self.customer == 'fav':
+                return self.price * 0.2
+            if self.customer == 'vip':
+                return self.price * 0.4
+
+
+# Правильный подход, один из способов.
+
+from abc import ABC, abstractmethod
+
+
+class Discount(ABC):
+
+    def __init__(self, customer, price):
+        self.customer = customer
+        self.price = price
+
+
+    @abstractmethod
+    def give_discount(self):
+    	pass
+
+
+class VIPDiscount(Discount):
+
+	def give_discount(self):
+		return self.price * 0.2
+
+
+
+class SuperVIPDiscount(VIPDiscount):
+
+    def get_discount(self):
+        return self.price * 0.4
